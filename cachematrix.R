1dcc72f82e109760c48f41434721b2550a45aa9f
Maksim Diakov <madyakov@edu.hse.ru>
2020-04-16 17:18:37 +0300

    Sumbission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The best function on the planet that creates an object that stores matrix and caches its inverse variant 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       getsolve = getsolve,
+       setsolve = setsolve)
 }
 
 
-## Write a short comment describing this function
+## The second best function on the planet that thinks twice (once) before calculating matrix inverse variant
+## from scratch as it may already store result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+        
 }
